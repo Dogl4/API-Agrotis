21e3832447e3ded902a9a39becc148d375bfa6a1
Pedro Douglas Pereira Barreto <doougllas@hotmail.com.br>
2022-06-24 12:34:13 -0300

    `feat`: Create `PropertyService` services.

---
+++ src/main/java/br/com/dogla/agrotis/services/PropertyService.java
@@ -0,0 +1,24 @@
+package br.com.dogla.agrotis.services;
+
+import br.com.dogla.agrotis.models.Property;
+import br.com.dogla.agrotis.repository.IProperty;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PropertyService {
+
+    @Autowired
+    private IProperty iProperty;
+
+    public List<Property> listAllProperties() {
+        return iProperty.findAll();
+    }
+
+    public Optional<Property> getByIdProperty(Long id) {
+        return iProperty.findById(id);
+    }
+}
