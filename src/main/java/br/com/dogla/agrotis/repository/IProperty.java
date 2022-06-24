adc1f133be8ecb622b2d5e7fb9cf2fc36ad52611
Pedro Douglas Pereira Barreto <doougllas@hotmail.com.br>
2022-06-24 12:31:42 -0300

    `feat`: Create `IProperty` repository/interface.

---
+++ src/main/java/br/com/dogla/agrotis/repository/IProperty.java
@@ -0,0 +1,10 @@
+package br.com.dogla.agrotis.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+
+import br.com.dogla.agrotis.models.Property;
+
+//@Repository
+public interface IProperty extends JpaRepository<Property, Long> {
+}
