590e6919b1d15830817ba0bcf7833b5f1d9b5f7e
Pedro Douglas Pereira Barreto <doougllas@hotmail.com.br>
2022-06-24 23:43:21 -0300

    fix: Insert `cpnj` column in `Solicitation` model.

--- src/main/java/br/com/dogla/agrotis/models/Solicitation.java
+++ src/main/java/br/com/dogla/agrotis/models/Solicitation.java
@@ -10,41 +10,30 @@ import javax.persistence.*;
 @Entity
 @Table(name = "solicitation")
 public class Solicitation {
-    @Id
-    @Column(name = "id", nullable = false)
-    private Long id;
+        @Id
+        @Column(name = "id", nullable = false)
+        private Long id;
 
-    @Column(name = "name", nullable = false)
-    private String nome;
+        @Column(name = "name", nullable = false)
+        private String nome;
 
-    @Column(name = "date_init", nullable = false)
-    private String dataInicial;
+        @Column(name = "date_init", nullable = false)
+        private String dataInicial;
 
-    @Column(name = "date_end",nullable = false)
-    private String dataFinal;
+        @Column(name = "date_end", nullable = false)
+        private String dataFinal;
 
-    @Column(name = "observation",nullable = false)
-    private String observacoes;
+        @Column(name = "cnpj", nullable = false)
+        private String cnpj;
 
-    @ManyToOne(cascade = CascadeType.REFRESH)
-    @JoinColumn(
-            name = "id_property",
-            nullable = false,
-            foreignKey = @ForeignKey(
-                    name = "fk_property_solicitation"
-            ),
-            referencedColumnName = "id"
-    )
-    private Property infoPropriedade;
+        @Column(name = "observation", nullable = false)
+        private String observacoes;
 
-    @ManyToOne(cascade = CascadeType.REFRESH)
-    @JoinColumn(
-            name="id_laboratory",
-            nullable = false,
-            foreignKey = @ForeignKey(
-                    name = "fk_laboratory_solicitation"
-            ),
-            referencedColumnName = "id"
-    )
-    private Property laboratorio;
+        @ManyToOne(cascade = CascadeType.REFRESH)
+        @JoinColumn(name = "id_property", nullable = false, foreignKey = @ForeignKey(name = "fk_property_solicitation"), referencedColumnName = "id")
+        private Property infoPropriedade;
+
+        @ManyToOne(cascade = CascadeType.REFRESH)
+        @JoinColumn(name = "id_laboratory", nullable = false, foreignKey = @ForeignKey(name = "fk_laboratory_solicitation"), referencedColumnName = "id")
+        private Property laboratorio;
 }
