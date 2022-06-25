a66f6c644e9d89d20be65829a08718a57ac7ac18
Pedro Douglas Pereira Barreto <doougllas@hotmail.com.br>
2022-06-25 05:26:28 -0300

    feat: Create `api/property`:(PUT and DELETE) routes.

--- src/main/java/br/com/dogla/agrotis/controllers/PropertyController.java
+++ src/main/java/br/com/dogla/agrotis/controllers/PropertyController.java
@@ -4,6 +4,7 @@ import br.com.dogla.agrotis.models.Property;
 import br.com.dogla.agrotis.services.PropertyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,5 +32,25 @@ public class PropertyController {
     @RequestMapping(value = "/{propertyId}", method = RequestMethod.GET)
     public Optional<Property> getById(@PathVariable("propertyId") Long propertyId) {
         return propertyService.getByIdProperty(propertyId);
+    }
+
+    @RequestMapping(value = "/{propertyId}", method = RequestMethod.PUT)
+    public ResponseEntity<?> update(@PathVariable("propertyId") Long propertyId, @RequestBody Property property) {
+        try {
+            Property property1 = propertyService.updateProperty(propertyId, property);
+            return ResponseEntity.status(HttpStatus.CREATED).body(property1);
+        } catch (Error efe) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(efe.getMessage());
+        }
+    }
+
+    @RequestMapping(value = "/{propertyId}", method = RequestMethod.DELETE)
+    public ResponseEntity<?> delete(@PathVariable("propertyId") Long propertyId) {
+        try {
+            propertyService.deleteByIdProperty(propertyId);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body("");
+        } catch (Error efe) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(efe.getMessage());
+        }
     }
 }
--- src/main/java/br/com/dogla/agrotis/services/PropertyService.java
+++ src/main/java/br/com/dogla/agrotis/services/PropertyService.java
@@ -1,10 +1,13 @@
 package br.com.dogla.agrotis.services;
 
 import br.com.dogla.agrotis.models.Property;
+import br.com.dogla.agrotis.models.Solicitation;
 import br.com.dogla.agrotis.repository.IProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.management.RuntimeErrorException;
+import javax.persistence.EntityExistsException;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,5 +27,24 @@ public class PropertyService {
 
     public Optional<Property> getByIdProperty(Long id) {
         return iProperty.findById(id);
+    }
+
+    public Property updateProperty(Long id, Property property) {
+        Property propertyDao = new Property();
+        if (iProperty.existsById(id)) {
+            propertyDao.setNome(property.getNome());
+            propertyDao.setId(property.getId());
+            return iProperty.save(propertyDao);
+        } else {
+            throw new java.lang.Error("Propriedade não encontrada.");
+        }
+    }
+
+    public void deleteByIdProperty(Long id) {
+        if (iProperty.existsById(id)) {
+            iProperty.deleteById(id);
+        } else {
+            throw new java.lang.Error("Propriedade nem existia.");
+        }
     }
 }
